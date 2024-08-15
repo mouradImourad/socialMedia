d713ae1a947ae80d408da6b77629da8a71d94e84
Mourad Mourad <mourad272@icloud.com>
2024-08-15 17:19:59 -0500

    Update Post API

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CreatePostView, PostListView, PostDetailView, UserPostListView
+from .views import CreatePostView, PostListView, PostDetailView, UserPostListView, PostUpdateView
 
 urlpatterns = [
     path('create/', CreatePostView.as_view(), name='create-post'),
     path('', PostListView.as_view(), name='post-list'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('user/<uuid:user_id>/', UserPostListView.as_view(), name='user-post-list'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from .models import Post
 from .serializers import PostSerializer
 
@@ -36,3 +37,16 @@ class UserPostListView(generics.ListAPIView):
     def get_queryset(self):
         user_id = self.kwargs['user_id']
         return Post.objects.filter(user_id=user_id)
+    
+
+
+class PostUpdateView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        post = self.get_object()
+        if post.user != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this post.")
+        serializer.save()

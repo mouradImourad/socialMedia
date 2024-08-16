5b2bb4d7230f2a3d41618f367d750797970c2624
Mourad Mourad <mourad272@icloud.com>
2024-08-16 18:38:40 -0500

    fetch conversations between users api

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,7 +1,10 @@
 # messages/urls.py
 from django.urls import path
-from .views import SendMessageView
+from .views import SendMessageView, RetrieveConversationView
 
 urlpatterns = [
     path('send/', SendMessageView.as_view(), name='send-message'),
+    path('conversation/<uuid:pk>/', RetrieveConversationView.as_view(), name='retrieve-conversation'),
 ]
+
+
--- messenger/views.py
+++ messenger/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Message
-from .serializers import MessageSerializer
+from .serializers import MessageSerializer, ConversationSerializer
+from django.contrib.auth import get_user_model
+
 
 
 
@@ -21,4 +23,17 @@ class SendMessageView(generics.CreateAPIView):
         serializer.save(sender=self.request.user)
 
 
-# Create your views here.
+
+
+User = get_user_model()
+
+class RetrieveConversationView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        return context
+

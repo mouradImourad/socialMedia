46924bf861302ff3d46d3d43cc9e9c70f52bd50f
Mourad Mourad <mourad272@icloud.com>
2024-08-16 19:42:28 -0500

    improve conversation handling by adding pagination

--- messenger/serializers.py
+++ messenger/serializers.py
@@ -5,7 +5,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-
 User = get_user_model()
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -13,8 +12,6 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = ['id', 'sender', 'recipient', 'content', 'timestamp', 'is_read']
         read_only_fields = ['id', 'timestamp', 'is_read', 'sender']
-
-
 
 class ConversationSerializer(serializers.ModelSerializer):
     messages = MessageSerializer(many=True)
@@ -26,18 +23,12 @@ class ConversationSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         request = self.context.get('request')
         user = request.user
-
-        # Retrieve messages between the logged-in user and the other user
         messages = Message.objects.filter(
             models.Q(sender=user, recipient=instance) |
             models.Q(sender=instance, recipient=user)
         ).order_by('timestamp')
-
-        # Mark messages as read when the recipient retrieves them
         unread_messages = messages.filter(recipient=user, is_read=False)
         unread_messages.update(is_read=True)
-
-        # Return the conversation details with the messages
         return {
             'id': instance.id,
             'username': instance.username,
--- messenger/views.py
+++ messenger/views.py
@@ -5,7 +5,9 @@ from rest_framework.exceptions import PermissionDenied
 from .models import Message
 from .serializers import MessageSerializer, ConversationSerializer
 from django.contrib.auth import get_user_model
-
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from django.db import models
 
 
 
@@ -25,19 +27,53 @@ class SendMessageView(generics.CreateAPIView):
 
 
 
+class MessagePagination(PageNumberPagination):
+    page_size = 10  # Number of messages per page
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'messages': data
+        })
+
 
 User = get_user_model()
 
 class RetrieveConversationView(generics.RetrieveAPIView):
     queryset = User.objects.all()
-    serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = MessagePagination
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context.update({"request": self.request})
         return context
 
+    def retrieve(self, request, *args, **kwargs):
+        user = request.user
+        instance = self.get_object()
+
+        messages = Message.objects.filter(
+            models.Q(sender=user, recipient=instance) |
+            models.Q(sender=instance, recipient=user)
+        ).order_by('timestamp')
+
+        # Mark unread messages as read
+        unread_messages = messages.filter(recipient=user, is_read=False)
+        unread_messages.update(is_read=True)
+
+        # Apply pagination to messages
+        paginator = MessagePagination()
+        paginated_messages = paginator.paginate_queryset(messages, request)
+
+        return paginator.get_paginated_response(MessageSerializer(paginated_messages, many=True).data)
+
+    
+    
 
 
 

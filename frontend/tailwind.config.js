9c50b05b8cd421e6e43acbdbaed71b28048eadf8
Mourad Mourad <mourad272@icloud.com>
2024-08-19 00:52:23 -0500

    all the dependencies installed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,css}", 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+

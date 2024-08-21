52432b9d97b27108bab560b42f5f5a77c3bd61dd
Mourad Mourad <mourad272@icloud.com>
2024-08-21 10:31:20 -0500

    homepage layout

---
+++ frontend/src/components/RightSidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Sidebar.css';
+
+const RightSidebar = () => {
+    return (
+        <div className="right-sidebar">
+            <h3>Suggestions</h3>
+            {/* Add suggested friends, trends later */}
+        </div>
+    );
+};
+
+export default RightSidebar;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import LeftSidebar from '../components/LeftSidebar';
+import Feed from '../components/Feed';
+import RightSidebar from '../components/RightSidebar';
+import './HomePage.css';
 
 
 const HomePage = () => {
-  return (
-    <div>
-       <Navbar /> 
-      <h1>Welcome to Lifex Home Page</h1>
-    </div>
-  );
+    return (
+        <div className="homepage">
+            <Navbar />
+            <div className="main-content">
+                <LeftSidebar />
+                <Feed />
+                <RightSidebar />
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
